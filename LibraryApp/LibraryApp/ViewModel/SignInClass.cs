91d42576a9f0d185b263e2a3fee88bc5f4859186
Michal <michal.ciupak0@gmail.com>
2024-01-24 02:18:30 +0200

    add log in

--- LibraryApp/LibraryApp/ViewModel/SignInClass.cs
+++ LibraryApp/LibraryApp/ViewModel/SignInClass.cs
@@ -1,29 +1,54 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using LibraryGrpc;
+using System.ComponentModel;
+using System.Net.Http;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace LibraryApp.ViewModel
 {
-	public class SignInClass
-	{
-		public string PropertySignUp { get; } = "Sign Up!";
-        public ICommand goBack => new Command(() => Application.Current.MainPage.Navigation.PopAsync());
-		public ICommand Login { set; get; }
-		public ICommand LabelTappedCommand => new Command(() => Application.Current.MainPage.Navigation.PushAsync(new SignUp()));
-		public string email;
-		public string Email
-		{
-			get { return email; }
-			set
-			{
+    public class SignInClass : INotifyPropertyChanged
+    {
+        private GrpcChannel channel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string propertySignUp = "Sign Up!";
+        public string PropertySignUp
+        {
+            get { return propertySignUp; }
+        }
+
+        private string messageSignIn = " ";
+        public string MessageSignIn
+        {
+            get { return messageSignIn; }
+            set
+            {
+                if (messageSignIn != value)
+                {
+                    messageSignIn = value;
+                    OnPropertyChanged(nameof(MessageSignIn));
+                }
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
                 if (email != value)
                 {
                     email = value;
+                    OnPropertyChanged(nameof(Email));
                 }
             }
+        }
 
-		}
-		public string password;
+        private string password;
         public string Password
         {
             get { return password; }
@@ -32,22 +57,52 @@ namespace LibraryApp.ViewModel
                 if (password != value)
                 {
                     password = value;
+                    OnPropertyChanged(nameof(Password));
                 }
             }
-
         }
+
+        public ICommand GoBack => new Command(() => Application.Current.MainPage.Navigation.PopAsync());
+        public ICommand Login { set; get; }
+        public ICommand LabelTappedCommand => new Command(() => Application.Current.MainPage.Navigation.PushAsync(new SignUp()));
+
         public SignInClass()
-		{
-            Login = new Command(onSubmit);
-		}
-        public void onSubmit()
         {
-            if (email == "dupa")
+            Login = new Command(OnSubmit);
+        }
+
+        public void OnSubmit()
+        {
+            if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
             {
-                //TODO
-                Application.Current.MainPage.Navigation.PushAsync(new ListItems());
+                channel = GrpcChannel.ForAddress("https://libraryappgrpc.azurewebsites.net", new GrpcChannelOptions
+                {
+                    HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+                });
+
+                var customerClient = new AuthIt.AuthItClient(channel);
+                var signInRequest = new LogInRequest { Email = email, Password = password };
+
+                var signInResponse = customerClient.LogInUser(signInRequest);
+
+                if (signInResponse.IsSuccess)
+                {
+                    Application.Current.MainPage.Navigation.PushAsync(new ListItems());
+                }
+                else
+                {
+                    MessageSignIn = signInResponse.Message;
+                }
+            }
+            else
+            {
+                MessageSignIn = "Fill the data";
             }
         }
-	}
-}
 
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
