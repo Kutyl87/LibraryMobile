8c8b719bc6421271856a23d2b5cbfcd6b671195d
Michal <michal.ciupak0@gmail.com>
2024-01-24 02:47:51 +0200

    add registration

--- LibraryApp/LibraryApp/ViewModel/SignUpClass.cs
+++ LibraryApp/LibraryApp/ViewModel/SignUpClass.cs
@@ -1,45 +1,21 @@
-﻿using System;
+﻿using System.ComponentModel;
+using System.Net.Http;
 using System.Windows.Input;
-using Google.Api;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using LibraryGrpc;
 using Xamarin.Forms;
 
 namespace LibraryApp.ViewModel
 {
-	public class SignUpClass
-	{
-        public ICommand goBack => new Command(() => Application.Current.MainPage.Navigation.PopAsync());
-		public ICommand SignIn => new Command(() => Application.Current.MainPage.Navigation.PushAsync(new SignIn()));
-        public ICommand Register { get; set; }
-		public string name;
-		public string surname;
-		public string email;
-		public string password;
-		public string repeatPassword;
-        public string Email
-        {
-            get { return email; }
-            set
-            {
-                if (email != value)
-                {
-                    email = value;
-                }
-            }
+    public class SignUpClass : INotifyPropertyChanged
+    {
+        private GrpcChannel channel;
 
-        }
-        public string Password
-        {
-            get { return password; }
-            set
-            {
-                if (password != value)
-                {
-                    password = value;
-                }
-            }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        }
-        public string  Name
+        private string name;
+        public string Name
         {
             get { return name; }
             set
@@ -47,10 +23,12 @@ namespace LibraryApp.ViewModel
                 if (name != value)
                 {
                     name = value;
+                    OnPropertyChanged(nameof(Name));
                 }
             }
-
         }
+
+        private string surname;
         public string Surname
         {
             get { return surname; }
@@ -59,10 +37,40 @@ namespace LibraryApp.ViewModel
                 if (surname != value)
                 {
                     surname = value;
+                    OnPropertyChanged(nameof(Surname));
                 }
             }
-
         }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged(nameof(Password));
+                }
+            }
+        }
+
+        private string repeatPassword;
         public string RepeatPassword
         {
             get { return repeatPassword; }
@@ -71,21 +79,73 @@ namespace LibraryApp.ViewModel
                 if (repeatPassword != value)
                 {
                     repeatPassword = value;
+                    OnPropertyChanged(nameof(RepeatPassword));
                 }
             }
+        }
 
-        }
-        public SignUpClass()
-		{
-            Register = new Command(onSubmit);
-        }
-        public void onSubmit()
+        private string messageSignIn = " ";
+        public string MessageSignIn
         {
-            if(email=="test")
+            get { return messageSignIn; }
+            set
             {
-                Application.Current.MainPage.Navigation.PushAsync(new ListItems());
+                if (messageSignIn != value)
+                {
+                    messageSignIn = value;
+                    OnPropertyChanged(nameof(MessageSignIn));
+                }
             }
         }
-	}
-}
 
+        public ICommand GoBack => new Command(() => Application.Current.MainPage.Navigation.PopAsync());
+        public ICommand SignIn => new Command(() => Application.Current.MainPage.Navigation.PushAsync(new SignIn()));
+        public ICommand Register { get; set; }
+
+        public SignUpClass()
+        {
+            Register = new Command(OnSubmit);
+        }
+
+        public void OnSubmit()
+        {
+            if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(surname) && password == repeatPassword)
+            {
+                channel = GrpcChannel.ForAddress("https://libraryappgrpc.azurewebsites.net", new GrpcChannelOptions
+                {
+                    HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+                });
+
+                var customerClient = new AuthIt.AuthItClient(channel);
+                var signUpRequest = new RegisterRequest
+                {
+                    Email = email,
+                    Name = name,
+                    Surname = surname,
+                    Password = password,
+                    ConfirmPassword = repeatPassword
+                };
+
+                var signUpResponse = customerClient.RegisterUser(signUpRequest);
+
+                if (signUpResponse.IsSuccess)
+                {
+                    Application.Current.MainPage.Navigation.PushAsync(new ListItems());
+                }
+                else
+                {
+                    MessageSignIn = signUpResponse.Message;
+                }
+            }
+            else
+            {
+                MessageSignIn = "Fill the data";
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
